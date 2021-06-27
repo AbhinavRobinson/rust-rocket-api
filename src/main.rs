0cb6f29eb3555e3ecd1fa23cc241787855031338
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-06-27 20:51:23 +0530

    (feat): add nightly rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![feature(decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, Rocket!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
