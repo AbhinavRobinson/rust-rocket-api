ef4eb7bb39c68214cd3f87f1ea5783a1b5ffec7a
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-07-05 12:00:00 +0530

    (feat): add doc

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+// enable macros
 #![feature(decl_macro)]
 
+// get macros from rocket
 #[macro_use]
 extern crate rocket;
 
+// get macro from get
 #[get("/")]
 fn index() -> &'static str {
     "Hello, Rocket!"
 }
 
+// init rocket server
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
 }
